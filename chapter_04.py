69690682ae33a717cfd37c04c5801753ba041cf6
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-12-17 17:21:00 +0330

    [Step_01]: chapter-04

---
+++ chapter_04.py
@@ -0,0 +1,26 @@
+# Try This: Hands-On Practice with Variables and Expressions
+
+# Variables and Naming Rules
+x = 2 + 4 * 5 - 6 / 3
+y = (2 + 4) * (5 - 6 / 3)
+print(x, y)
+
+# Manipulating Strings and Numbers
+num1 = 5
+num2 = 2.5
+str1 = "Hello"
+str2 = "World"
+result1 = str1 * num1
+result2 = str1 + str(num1)
+import math
+import cmath
+
+result3 = math.sqrt(25)
+result4 = cmath.sqrt(-4)
+print(result1, result2, result3, result4)
+
+# Getting User Input
+user_input = input("Enter an integer: ")
+num = int(user_input)
+user_input = input("Enter a float: ")
+num = float(user_input)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# The Quick Python Book - "Try This" Exercises
+
+## This repository contains the "Try This" exercises from The Quick Python Book. These exercises are designed to help you practice and understand the core concepts of Python programming as presented in the book.
+
+![alt text](image.png)
